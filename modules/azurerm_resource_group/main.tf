dcb9362a6fe8cbfbceb6566d86d5c8a74564187c
udbhav chauhan <udbhavchauhan97@gmail.com>
2025-12-25 23:17:47 +0530

    updated rg module

--- modules/azurerm_resource_group/main.tf
+++ modules/azurerm_resource_group/main.tf
@@ -1,8 +1,6 @@
-resource "azurerm_resource_group" "rgs" {
-	for_each = var.rgs
-
-	name     = each.value.name
-	location = each.value.location
-	managed_by = lookup(each.value, "managed_by", null)
-	tags     = lookup(each.value, "tags", {})
+resource "azurerm_resource_group" "rg" {
+  for_each = var.rgs
+  name     = each.value.name
+  location = each.value.location
+  tags     = each.value.tags
 }
--- modules/azurerm_resource_group/variables.tf
+++ modules/azurerm_resource_group/variables.tf
@@ -1,9 +1,7 @@
 variable "rgs" {
-  description = "Map of resource groups to create"
   type = map(object({
-    name       = string
-    location   = string
-    managed_by = optional(string)
-    tags       = optional(map(string))
+    name     = string
+    location = string
+    tags     = map(string)
   }))
 }
